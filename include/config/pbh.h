deb86c9a58189bff2b51b51be49eb0eb7f89c56a
Nexxo17 <tedestroyaruben@gmail.com>
2023-11-02 20:34:57 -0300

    BattleInterface Changes GFX
    Cambios graficos a la interfaz
    del jugador
    (INCOMPLETO)
    
    # Conflicts:
    #	credits/creditos.txt
    #	data/layouts/LittlerootTown/map.bin
    #	data/maps/AlteringCave/scripts.pory
    #	data/tilesets/secondary/petalburg/metatiles.bin
    #	pokeemerald-1.png
    #	pokeemerald-2.png
    #	pokeemerald-3.png
    
    # Conflicts:
    #	graphics/battle_interface/ball_status_bar.png
    #	graphics/battle_interface/healthbox_singles_opponent.png
    #	graphics/battle_interface/healthbox_singles_player.png
    #	graphics/battle_interface/textbox.png
    #	src/battle_interface.c

--- include/config/pbh.h
+++ include/config/pbh.h
@@ -5,6 +5,6 @@
 #define PBH_PALETAS_UNICAS              TRUE    // Si es TRUE, los sprites de los Pokémon (tanto los de batalla como en el OW) tendrán una variación (no demasiado grande, pero visible) según su personalidad. Créditos a CitrusBolt.
 #define PBH_ALMACENAMIENTO_RAPIDO       TRUE    // Si es TRUE, el movimiento entre cajas en el almacenamiento Pokémon va mucho más rápido. Créditos a LuckyTyphlosion.
 #define PBH_HUEVOS_COLOR_TIPO           TRUE    // Si es TRUE, los huevos tienen el color de los tipos del Pokémon que vaya dentro. Créditos a CitrusBolt.
-#define PBH_ICONOS_TIPOS_BATALLA        TRUE    // Si es TRUE, se mostrará el icono de tipo en batalla en vez de Tipo/..., el icono de categoría y la efectividad.
+#define PBH_ICONOS_TIPOS_BATALLA        FALSE    // Si es TRUE, se mostrará el icono de tipo en batalla en vez de Tipo/..., el icono de categoría y la efectividad.
 
 #endif // GUARD_CONFIG_PBH_H
