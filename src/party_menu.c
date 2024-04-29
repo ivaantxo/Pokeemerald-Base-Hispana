61ab35b85134abced9aa2f0f9d39438d6faad3f1
Ariel A <24759293+aarant@users.noreply.github.com>
2024-04-28 23:14:31 -0400

    Revert "Fixed multi battle partner party icons."
    This reverts commit 4741b328d379a34c9aa5eebd60630cad35baa173.

--- src/party_menu.c
+++ src/party_menu.c
@@ -1056,8 +1056,6 @@ static u8 *GetPartyMenuBgTile(u16 tileId)
     return &sPartyBgGfxTilemap[tileId << 5];
 }
 
-#define POKE_ICON_BASE_PAL_TAG 56000
-
 static void CreatePartyMonSprites(u8 slot)
 {
     u8 actualSlot;
@@ -1069,12 +1067,7 @@ static void CreatePartyMonSprites(u8 slot)
 
         if (gMultiPartnerParty[actualSlot].species != SPECIES_NONE)
         {
-            u8 index = slot < PARTY_SIZE ? IndexOfSpritePaletteTag(POKE_ICON_BASE_PAL_TAG + slot) : 0xFF;
             CreatePartyMonIconSpriteParameterized(gMultiPartnerParty[actualSlot].species, gMultiPartnerParty[actualSlot].personality, &sPartyMenuBoxes[slot], 0, FALSE);
-            if (index < 16) { // Like SetMonIconPalette, but by species & personality
-                LoadCompressedPalette(GetMonSpritePalFromSpeciesAndPersonality(gMultiPartnerParty[actualSlot].species, 0, gMultiPartnerParty[actualSlot].personality), index*16 + 0x100, 32);
-                gSprites[sPartyMenuBoxes[slot].monSpriteId].oam.paletteNum = index;
-            }
             CreatePartyMonHeldItemSpriteParameterized(gMultiPartnerParty[actualSlot].species, gMultiPartnerParty[actualSlot].heldItem, &sPartyMenuBoxes[slot]);
             CreatePartyMonPokeballSpriteParameterized(gMultiPartnerParty[actualSlot].species, &sPartyMenuBoxes[slot]);
             if (gMultiPartnerParty[actualSlot].hp == 0)
@@ -3937,6 +3930,8 @@ static bool8 SetUpFieldMove_Dive(void)
     }
     return FALSE;
 }
+
+#define POKE_ICON_BASE_PAL_TAG 56000
 
 static void CreatePartyMonIconSprite(struct Pokemon *mon, struct PartyMenuBox *menuBox, u32 slot)
 {
