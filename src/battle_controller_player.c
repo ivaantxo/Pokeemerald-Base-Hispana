f368867da915d2628f81aed7fd27f0374792dd20
ivaantxo <76601053+ivaantxo@users.noreply.github.com>
2024-09-15 16:13:29 +0200

    Update battle_controller_player.c

--- src/battle_controller_player.c
+++ src/battle_controller_player.c
@@ -1762,9 +1762,9 @@ static void MoveSelectionDisplayMoveDescription(u32 battler)
     u8 cat = gMovesInfo[move].category;
 
     u8 pwr_num[3], acc_num[3];
-    u8 cat_desc[7] = _("CAT: ");
-    u8 pwr_desc[7] = _("POT: ");
-    u8 acc_desc[7] = _("PRE: ");
+    u8 cat_desc[7] = _("Cat: ");
+    u8 pwr_desc[7] = _("Pot: ");
+    u8 acc_desc[7] = _("Pre: ");
     u8 cat_start[] = _("{CLEAR_TO 0x03}");
     u8 pwr_start[] = _("{CLEAR_TO 0x38}");
     u8 acc_start[] = _("{CLEAR_TO 0x6D}");
