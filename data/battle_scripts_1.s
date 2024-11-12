a06dc03207b1bbb5c9e67144fecfe4a3dcaf00be
hedara90 <90hedara@gmail.com>
2024-11-12 19:18:37 +0100

    Fixed curse + Protean interaction (#5663)
    Co-authored-by: Hedara <hedara90@gmail.com>

--- data/battle_scripts_1.s
+++ data/battle_scripts_1.s
@@ -4159,6 +4159,7 @@ BattleScript_EffectMinimize::
 BattleScript_EffectCurse::
 	jumpiftype BS_ATTACKER, TYPE_GHOST, BattleScript_GhostCurse
 	attackcanceler
+	jumpiftype BS_ATTACKER, TYPE_GHOST, BattleScript_DoGhostCurse
 	attackstring
 	ppreduce
 	jumpifstat BS_ATTACKER, CMP_GREATER_THAN, STAT_SPEED, MIN_STAT_STAGE, BattleScript_CurseTrySpeed
