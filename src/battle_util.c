fd397e9e1c2878111ce33484c1f76a000d9d0388
RavePossum <145081120+ravepossum@users.noreply.github.com>
2024-12-08 05:07:19 -0500

    Fix move category and category icon when PSS is off (#5786)

--- src/battle_util.c
+++ src/battle_util.c
@@ -11212,7 +11212,7 @@ u8 GetBattleMoveCategory(u32 moveId)
 
     if (IS_MOVE_STATUS(moveId))
         return DAMAGE_CATEGORY_STATUS;
-    return gTypesInfo[GetMoveType(gCurrentMove)].damageCategory;
+    return gTypesInfo[GetMoveType(moveId)].damageCategory;
 }
 
 static bool32 TryRemoveScreens(u32 battler)
