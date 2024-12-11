7eee3b35c2a5d607d5f3b2037dc96067052e5347
Eduardo Quezada <eduardo602002@gmail.com>
2024-12-11 05:05:09 -0300

    Added missing tests + Fix Coaching/Crafty Shield interaction (#5796)
    Co-authored-by: hedara90 <90hedara@gmail.com>

--- src/battle_util.c
+++ src/battle_util.c
@@ -8633,8 +8633,8 @@ bool32 IsBattlerProtected(u32 battlerAtk, u32 battlerDef, u32 move)
      && IsMoveMakingContact(move, gBattlerAttacker)
      && GetBattlerAbility(gBattlerAttacker) == ABILITY_UNSEEN_FIST)
         return FALSE;
-    else if (gSideStatuses[GetBattlerSide(battlerDef)] & SIDE_STATUS_CRAFTY_SHIELD
-             && IS_MOVE_STATUS(move))
+    else if ((gSideStatuses[GetBattlerSide(battlerDef)] & SIDE_STATUS_CRAFTY_SHIELD)
+             && IS_MOVE_STATUS(move) && gMovesInfo[move].effect != EFFECT_COACHING)
         return TRUE;
     else if (gMovesInfo[move].ignoresProtect)
         return FALSE;
