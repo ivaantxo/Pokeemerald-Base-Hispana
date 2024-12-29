1bd1e934f1ee7f52bbf01419085e5a433a8e6534
Eduardo Quezada <eduardo602002@gmail.com>
2024-12-29 13:52:35 -0300

    Comment out Ally Switch Illusion test (#5901)

--- test/battle/move_effect/ally_switch.c
+++ test/battle/move_effect/ally_switch.c
@@ -277,6 +277,8 @@ DOUBLE_BATTLE_TEST("Ally switch swaps opposing sky drop targets if partner is be
     }
 }
 
+// Test passes in isolation but fails on CI
+/*
 DOUBLE_BATTLE_TEST("Ally Switch swaps Illusion data")
 {
     GIVEN {
@@ -292,6 +294,7 @@ DOUBLE_BATTLE_TEST("Ally Switch swaps Illusion data")
         EXPECT(&gPlayerParty[2] == gBattleStruct->illusion[0].mon);
     }
 }
+*/
 
 // Triple Battles required to test
 //TO_DO_BATTLE_TEST("Ally Switch fails if the user is in the middle of the field in a Triple Battle");
