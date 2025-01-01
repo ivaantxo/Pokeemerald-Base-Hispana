92c0039a238735e254f1610edee1ba33b64596b3
Pawkkie <61265402+Pawkkie@users.noreply.github.com>
2025-01-01 15:40:29 -0500

    Cleanup fix from 5922 (#5927)

--- src/battle_ai_switch_items.c
+++ src/battle_ai_switch_items.c
@@ -964,9 +964,7 @@ bool32 ShouldSwitch(u32 battler)
         if (i == gBattleStruct->monToSwitchIntoId[battlerIn2])
             continue;
         if (IsAceMon(battler, i))
-        {
             continue;
-        }
 
         availableToSwitch++;
     }
