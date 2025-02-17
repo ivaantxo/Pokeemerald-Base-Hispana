444437274c5cfa44ab4972b5fc685c946c9bca40
Salem <65783283+u8-Salem@users.noreply.github.com>
2025-02-17 10:02:28 +0100

    remove obsolete check for steven when retrieving partner name (#6283)

--- src/pokemon.c
+++ src/pokemon.c
@@ -6056,15 +6056,8 @@ const u8 *GetTrainerPartnerName(void)
 {
     if (gBattleTypeFlags & BATTLE_TYPE_INGAME_PARTNER)
     {
-        if (gPartnerTrainerId == TRAINER_PARTNER(PARTNER_STEVEN))
-        {
-            return GetTrainerNameFromId(TRAINER_STEVEN);
-        }
-        else
-        {
-            GetFrontierTrainerName(gStringVar1, gPartnerTrainerId);
-            return gStringVar1;
-        }
+        GetFrontierTrainerName(gStringVar1, gPartnerTrainerId);
+        return gStringVar1;
     }
     else
     {
