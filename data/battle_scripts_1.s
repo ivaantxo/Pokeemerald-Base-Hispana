446521f20075e3431174e59c79ccdf502871f92d
Alex <93446519+AlexOn1ine@users.noreply.github.com>
2025-04-15 14:06:35 +0200

    Fix regression from EndTurnRefactor (#6616)

--- data/battle_scripts_1.s
+++ data/battle_scripts_1.s
@@ -9749,7 +9749,7 @@ BattleScript_DamageNonTypesContinues::
 	printfromtable gDamageNonTypesDmgStringIds
 	waitmessage B_WAIT_TIME_LONG
 	effectivenesssound
-	hitanimation BS_SCRIPTING
+	hitanimation BS_ATTACKER
 	goto BattleScript_DoTurnDmg
 
 BattleScript_EffectTryReducePP::
