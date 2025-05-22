942a533ea1c4dacbe941de20e21eb2ef63eb76c0
Eduardo Quezada <eduardo602002@gmail.com>
2025-05-22 04:24:49 -0400

    Added missing root folder check in trainer battle type migration script (#6944)

--- migration_scripts/1.12/convert_trainer_battle_types.py
+++ migration_scripts/1.12/convert_trainer_battle_types.py
@@ -1,10 +1,15 @@
 import re
+import os
 
 def trainer_battle_types(data):
     data = re.sub(re.escape("Double Battle: No"), "Battle Type: Singles", data)
     data = re.sub(re.escape("Double Battle: Yes"), "Battle Type: Doubles", data)
 
     return data
+
+if not os.path.exists("Makefile"):
+    print("Please run this script from your root folder.")
+    quit()
 
 with open('src/data/trainers.party', 'r') as file:
     data = file.read()
