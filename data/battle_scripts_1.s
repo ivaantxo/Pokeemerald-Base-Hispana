5bdab5163e82d833a24c003f4a0b365979f3a23c
Alex <93446519+AlexOn1ine@users.noreply.github.com>
2025-05-30 11:58:32 +0200

    Update Supersweet Syrup with eject pack fix (#7015)

--- data/battle_scripts_1.s
+++ data/battle_scripts_1.s
@@ -7847,8 +7847,9 @@ BattleScript_SupersweetSyrupLoopIncrement:
 	jumpifbytenotequal gBattlerTarget, gBattlersCount, BattleScript_SupersweetSyrupLoop
 	copybyte sBATTLER, gBattlerAttacker
 	destroyabilitypopup
- 	restoretarget
+	restoretarget
 	pause B_WAIT_TIME_MED
+	tryintimidatejectpack
 	end3
 
 BattleScript_SupersweetSyrupWontDecrease:
