5579ccc11b17f717f6fb3c9c3c19ba137b2c5f87
J2M2 <juan.martinezmoritz@hotmail.com>
2025-06-17 20:28:45 +0200

    Refactor battle text handling for stat changes to improve Spanish language support

--- src/battle_script_commands.c
+++ src/battle_script_commands.c
@@ -12391,23 +12391,23 @@ static u32 ChangeStatBuffs(s8 statValue, u32 statId, u32 flags, const u8 *BS_ptr
                 statValue = -2;
             gBattleTextBuff2[0] = B_BUFF_PLACEHOLDER_BEGIN;
             index = 1;
-            if (statValue == -2)
-            {
-                gBattleTextBuff2[1] = B_BUFF_STRING;
-                gBattleTextBuff2[2] = STRINGID_STATHARSHLY;
-                gBattleTextBuff2[3] = STRINGID_STATHARSHLY >> 8;
-                index = 4;
-            }
-            else if (statValue <= -3)
-            {
-                gBattleTextBuff2[1] = B_BUFF_STRING;
-                gBattleTextBuff2[2] = STRINGID_SEVERELY & 0xFF;
-                gBattleTextBuff2[3] = STRINGID_SEVERELY >> 8;
-                index = 4;
-            }
+            // Spanish: verb first, then adverb
             gBattleTextBuff2[index++] = B_BUFF_STRING;
             gBattleTextBuff2[index++] = STRINGID_STATFELL;
             gBattleTextBuff2[index++] = STRINGID_STATFELL >> 8;
+            
+            if (statValue == -2)
+            {
+                gBattleTextBuff2[index++] = B_BUFF_STRING;
+                gBattleTextBuff2[index++] = STRINGID_STATHARSHLY;
+                gBattleTextBuff2[index++] = STRINGID_STATHARSHLY >> 8;
+            }
+            else if (statValue <= -3)
+            {
+                gBattleTextBuff2[index++] = B_BUFF_STRING;
+                gBattleTextBuff2[index++] = STRINGID_SEVERELY & 0xFF;
+                gBattleTextBuff2[index++] = STRINGID_SEVERELY >> 8;
+            }
             gBattleTextBuff2[index] = B_BUFF_EOS;
 
             if (gBattleMons[battler].statStages[statId] == MIN_STAT_STAGE)
@@ -12431,23 +12431,23 @@ static u32 ChangeStatBuffs(s8 statValue, u32 statId, u32 flags, const u8 *BS_ptr
             statValue = 2;
         gBattleTextBuff2[0] = B_BUFF_PLACEHOLDER_BEGIN;
         index = 1;
-        if (statValue == 2)
-        {
-            gBattleTextBuff2[1] = B_BUFF_STRING;
-            gBattleTextBuff2[2] = STRINGID_STATSHARPLY;
-            gBattleTextBuff2[3] = STRINGID_STATSHARPLY >> 8;
-            index = 4;
-        }
-        else if (statValue >= 3)
-        {
-            gBattleTextBuff2[1] = B_BUFF_STRING;
-            gBattleTextBuff2[2] = STRINGID_DRASTICALLY & 0xFF;
-            gBattleTextBuff2[3] = STRINGID_DRASTICALLY >> 8;
-            index = 4;
-        }
+        // Spanish: verb first, then adverb
         gBattleTextBuff2[index++] = B_BUFF_STRING;
         gBattleTextBuff2[index++] = STRINGID_STATROSE;
         gBattleTextBuff2[index++] = STRINGID_STATROSE >> 8;
+        
+        if (statValue == 2)
+        {
+            gBattleTextBuff2[index++] = B_BUFF_STRING;
+            gBattleTextBuff2[index++] = STRINGID_STATSHARPLY;
+            gBattleTextBuff2[index++] = STRINGID_STATSHARPLY >> 8;
+        }
+        else if (statValue >= 3)
+        {
+            gBattleTextBuff2[index++] = B_BUFF_STRING;
+            gBattleTextBuff2[index++] = STRINGID_DRASTICALLY & 0xFF;
+            gBattleTextBuff2[index++] = STRINGID_DRASTICALLY >> 8;
+        }
         gBattleTextBuff2[index] = B_BUFF_EOS;
 
         if (gBattleMons[battler].statStages[statId] == MAX_STAT_STAGE)
