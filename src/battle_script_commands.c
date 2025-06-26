4def635e1f4c796a72e45b0aa9993e49c922df0e
Alex <93446519+AlexOn1ine@users.noreply.github.com>
2025-06-26 15:53:50 +0200

    Fixes wrong assignment in TrySymbiosis (#7221)

--- src/battle_script_commands.c
+++ src/battle_script_commands.c
@@ -9181,7 +9181,6 @@ static bool32 TrySymbiosis(u32 battler, u32 itemId)
         BestowItem(BATTLE_PARTNER(battler), battler);
         gLastUsedAbility = gBattleMons[BATTLE_PARTNER(battler)].ability;
         gBattleScripting.battler = gBattlerAbility = BATTLE_PARTNER(battler);
-        gBattlerAttacker = battler;
         BattleScriptPush(gBattlescriptCurrInstr + 2);
         gBattlescriptCurrInstr = BattleScript_SymbiosisActivates;
         return TRUE;
