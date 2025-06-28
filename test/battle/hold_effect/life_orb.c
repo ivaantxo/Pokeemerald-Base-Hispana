0c4b0c582ea84509ab11a58589a5bb40fe34293d
ghoulslash <41651341+ghoulslash@users.noreply.github.com>
2025-06-28 04:20:39 -0400

    Add some tests (#7234)

--- test/battle/hold_effect/life_orb.c
+++ test/battle/hold_effect/life_orb.c
@@ -31,6 +31,23 @@ SINGLE_BATTLE_TEST("Life Orb activates if it hits a Substitute")
     }
 }
 
+SINGLE_BATTLE_TEST("Life Orb does not activate if using status move on a Substitute")
+{
+    GIVEN {
+        PLAYER(SPECIES_WOBBUFFET) { Item(ITEM_LIFE_ORB); }
+        OPPONENT(SPECIES_WOBBUFFET);
+    } WHEN {
+        TURN { MOVE(opponent, MOVE_SUBSTITUTE); MOVE(player, MOVE_GROWL); }
+    } SCENE {
+        ANIMATION(ANIM_TYPE_MOVE, MOVE_SUBSTITUTE, opponent);
+        ANIMATION(ANIM_TYPE_MOVE, MOVE_GROWL, player);
+        NONE_OF {
+            HP_BAR(player);
+            MESSAGE("Wobbuffet was hurt by the Life Orb!");
+        }
+    }
+}
+
 SINGLE_BATTLE_TEST("Life Orb does not activate if using a status move")
 {
     GIVEN {
