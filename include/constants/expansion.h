2ab1bc87e0d019a7ffd2de6f2afcce68d289ab94
Hedara <hedara90@gmail.com>
2025-08-28 16:46:58 +0200

    Start of 1.14 cycle

--- include/constants/expansion.h
+++ include/constants/expansion.h
@@ -3,8 +3,8 @@
 
 // Last version: 1.13.0
 #define EXPANSION_VERSION_MAJOR 1
-#define EXPANSION_VERSION_MINOR 13
-#define EXPANSION_VERSION_PATCH 1
+#define EXPANSION_VERSION_MINOR 14
+#define EXPANSION_VERSION_PATCH 0
 
 // FALSE if this this version of Expansion is not a tagged commit, i.e.
 // it contains unreleased changes.
