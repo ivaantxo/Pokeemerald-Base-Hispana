593219226b7a739b3290a26d73140e7798b8c05d
FosterProgramming <jamiefosterandco@gmail.com>
2025-10-16 12:06:54 +0200

    Fix dns color transition not applying weather blending (#7883)
    Co-authored-by: Bassoonian <iasperbassoonian@gmail.com>

--- src/overworld.c
+++ src/overworld.c
@@ -1716,8 +1716,7 @@ static void OverworldBasic(void)
          || bld0[1] != bld1[1]
          || bld0[2] != bld1[2])
         {
-           UpdateAltBgPalettes(PALETTES_BG);
-           UpdatePalettesWithTime(PALETTES_ALL);
+           ApplyWeatherColorMapIfIdle(gWeatherPtr->colorMapIndex);
         }
     }
 }
