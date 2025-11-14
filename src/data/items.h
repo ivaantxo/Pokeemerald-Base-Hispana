90c5e1424f29c4d461895bc17e6c807ab91ed0f9
Montblanc <montmoguri@gmail.com>
2025-11-14 02:42:51 -0800

    Removed extra period in Pokéshi Doll description (#8252)

--- src/data/items.h
+++ src/data/items.h
@@ -15048,7 +15048,7 @@ const struct Item gItemsInfo[] =
         .price = 2000,
         .description = COMPOUND_STRING(
             "A wooden toy\n"
-            "resembling a Poké-.\n"
+            "resembling a Poké-\n"
             "mon. Can be sold."),
         .pocket = POCKET_ITEMS,
         .sortType = ITEM_TYPE_SELLABLE,
