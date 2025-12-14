1209666e5a3b3999f5b520800963c3d0e009d3ce
Alex <93446519+AlexOn1ine@users.noreply.github.com>
2025-12-14 12:42:55 +0100

    Fix battler side status check in CalcCritChanceStage (#8524)

--- src/battle_script_commands.c
+++ src/battle_script_commands.c
@@ -1613,7 +1613,7 @@ s32 CalcCritChanceStage(u32 battlerAtk, u32 battlerDef, u32 move, bool32 recordA
 {
     s32 critChance = 0;
 
-    if (gSideStatuses[battlerDef] & SIDE_STATUS_LUCKY_CHANT)
+    if (gSideStatuses[GetBattlerSide(battlerDef)] & SIDE_STATUS_LUCKY_CHANT)
     {
         critChance = CRITICAL_HIT_BLOCKED;
     }
