b0285152b480c3c1211d8cdcc1bccfa38444300f
PhallenTree <168426989+PhallenTree@users.noreply.github.com>
2025-12-17 10:53:19 +0000

    Allow Floette Eternal to mega evolve (#8558)

--- src/data/pokemon/species_info/gen_6_families.h
+++ src/data/pokemon/species_info/gen_6_families.h
@@ -2222,6 +2222,7 @@ const struct SpeciesInfo gSpeciesInfoGen6[] =
         .backPicYOffset = 2,
         .levelUpLearnset = sFloetteEternalLevelUpLearnset,
         .teachableLearnset = sFloetteEternalTeachableLearnset,
+        .formChangeTable = sFloetteEternalFormChangeTable,
     },
 
 #define FLORGES_MISC_INFO(Form, iconPal)                                        \
